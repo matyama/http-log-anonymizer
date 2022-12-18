ab38b937cf545c78e20e314f6ff777308b557680
Martin Matyášek <martin.matyasek@gmail.com>
2022-12-18 19:41:25 +0100

    Add IPv6 handling to `anonymize_ip`
    This commit finishes some unresolved TODOs in the application logic.
    Specifically it adds anonymization of IPv6 addresses.
    
    Known issues & limitations:
     - Note that the due to Rust's default formatting, the non-anonymized
       part of the original IPv6 address might be shortened (but still
       representing the same address, modulo the hidden part)

--- anonymizer/src/lib.rs
+++ anonymizer/src/lib.rs
@@ -1,22 +1,31 @@
 use itertools::Itertools;
 use std::net::IpAddr;
 
-// TODO: better API, possibly using Into
+/// Anonymize an IP address by replacing the last octet with 'x'.
+///
+/// Any non-IP input is returned unchanged.
+///
+/// # Example
+/// ```
+/// # use anonymizer::anonymize_ip;
+/// assert_eq!(&anonymize_ip("1.2.3.4".to_string()), "1.2.3.x");
+/// ```
 pub fn anonymize_ip(ip: String) -> String {
     let Ok(ip) = ip.parse() else {
-        // TODO: check what's the expected behavior here (currntly non-IPs are _not_ anonymized)
+        // XXX: check what's the expected behavior here (currntly non-IPs are _not_ anonymized)
         return ip;
     };
 
     match ip {
         IpAddr::V4(ip) => {
-            // TODO: optimize (e.g. no allocs)
             let mut ip = ip.octets().into_iter().take(3).join(".");
             ip.push_str(".x");
             ip
         }
-        IpAddr::V6(_ip) => {
-            todo!("not implemented yet")
+        IpAddr::V6(ip) => {
+            let mut ip = ip.to_string().split(":").take(8).join(":");
+            ip.push_str(":xxxx");
+            ip
         }
     }
 }
@@ -26,9 +35,12 @@ mod tests {
     use super::*;
     use rstest::*;
 
-    // TODO: tests for IPv6
     #[rstest]
-    #[case::valid("1.2.3.4".to_string(), "1.2.3.x".to_string())]
+    #[case::valid_v4("1.2.3.4".to_string(), "1.2.3.x".to_string())]
+    #[case::valid_v6(
+        "2001:0db8:85a3:0000:0000:8a2e:0370:7334".to_string(),
+        "2001:db8:85a3::8a2e:370:7334:xxxx".to_string(),
+    )]
     #[case::invalid("a.b.c.d".to_string(), "a.b.c.d".to_string())]
     fn ip_anonymization(#[case] ip: String, #[case] expected: String) {
         assert_eq!(anonymize_ip(ip), expected);
