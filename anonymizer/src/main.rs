58b0a798e69a2a12287541e40dd36b52cd93b294
Martin Matyášek <martin.matyasek@gmail.com>
2022-12-18 11:06:27 +0100

    Introduce `ClickHouseSink` and `make_table`
    This commit refactors the ClickHouse import logic in following ways:
     - Inserter no longer runs async with respect to processing the log
       stream. Consequentially, writes happen in order of the input
       messages.
     - New `ClickHouseSink` is added as a single point of writes for all the
       consumers. The idea is that it can 1) track the latest offsets for
       each partition (consumer in the group) and report them back on
       `Inserter` commit.
     - Consumers initiate writes on the sink and get back
       pending/success/failure result. In case of a success, received
       offsets are committed to Kafka cluster.
    
    Second important feature this commit adds is that the app check at
    startup whether the log table exists in ClickHouse and creates it if
    necessary.
    
    Known issues & limitations:
     - Sink still hits the rate limiting done by the ch-proxy, this might be
       either due to some requests done in the background of the Inserter or
       just by bad config/setup.
     - Sink does not yet implement graceful shutdown which would flush
       remaining inserts (and some consumer could commit last offsets)
     - Errors remain generally unhandled and the app will mostly just panic

--- anonymizer/src/main.rs
+++ anonymizer/src/main.rs
@@ -3,13 +3,16 @@ use std::time::Duration;
 
 use anonymizer::anonymize_ip;
 use capnp::{message::ReaderOptions, serialize::read_message_from_flat_slice};
+use clickhouse::inserter::Inserter;
 use clickhouse::Row;
 use futures::stream::FuturesUnordered;
 use futures::{StreamExt, TryStreamExt};
+use rdkafka::consumer::CommitMode;
 use rdkafka::consumer::{stream_consumer::StreamConsumer, Consumer, ConsumerContext};
 use rdkafka::error::KafkaResult;
 use rdkafka::message::{Message, OwnedMessage};
 use rdkafka::topic_partition_list::TopicPartitionList;
+use rdkafka::Offset;
 use serde::{Deserialize, Serialize};
 use time::OffsetDateTime;
 use tokio::sync::Mutex;
@@ -36,15 +39,25 @@ struct ClickHouseConfig {
     user: String,
     password: String,
     database: String,
+    tcp_keepalive: u64,
+    pool_idle_timeout: u64,
     log_table: String,
     max_entries: u64,
     insert_period: Option<u64>,
 }
 
 impl From<&ClickHouseConfig> for clickhouse::Client {
-    #[inline]
     fn from(cfg: &ClickHouseConfig) -> Self {
-        clickhouse::Client::default()
+        use hyper::client::connect::HttpConnector;
+
+        let mut connector = HttpConnector::new();
+        connector.set_keepalive(Some(Duration::from_secs(cfg.tcp_keepalive)));
+
+        let client = hyper::Client::builder()
+            .pool_idle_timeout(Duration::from_secs(cfg.pool_idle_timeout))
+            .build(connector);
+
+        clickhouse::Client::with_http_client(client)
             .with_url(&cfg.url)
             .with_user(&cfg.user)
             .with_password(&cfg.password)
@@ -83,6 +96,22 @@ impl Config {
             .try_deserialize()
     }
 }
+
+const TABLE_DDL: &str = "
+      CREATE TABLE IF NOT EXISTS ? (
+        timestamp DateTime NOT NULL,
+        resource_id UInt64 NOT NULL,
+        bytes_sent UInt64 NOT NULL,
+        request_time_milli UInt64 NOT NULL,
+        response_status UInt16 NOT NULL,
+        cache_status LowCardinality(String) NOT NULL,
+        method LowCardinality(String) NOT NULL,
+        remote_addr String NOT Null,
+        url String NOT NULL
+      )
+      ENGINE = ReplacingMergeTree
+      PARTITION BY toYYYYMM(timestamp)
+      ORDER BY (resource_id, response_status, remote_addr, timestamp)";
 
 // TODO: ClickHouse works with `&str` or
 //  - https://docs.rs/smartstring/latest/smartstring/struct.SmartString.html
@@ -163,21 +192,98 @@ impl ConsumerContext for HttpLogConsumerContext {
 
 type HttpLogConsumer = StreamConsumer<HttpLogConsumerContext>;
 
+enum ImportResult<E> {
+    /// Insert is in progress, buffering the logs
+    Pending,
+    /// Inserts have been written and committed, returning last offsets as [`TopicPartitionList`]
+    Success(TopicPartitionList),
+    /// Insert has failed with error `E`
+    Failure(E),
+}
+
+struct ClickHouseSink {
+    /// Single Kafka topic that's being consumed
+    topic: String,
+    /// Offsets of the last written logs for each partition and [topic]
+    tpl: TopicPartitionList,
+    /// ClickHouse [`Inserter`](clickhouse::inserter::Inserter) for [`HttpLog`]
+    inserter: Inserter<HttpLog>,
+}
+
+impl ClickHouseSink {
+    #[inline]
+    fn new(topic: String, inserter: Inserter<HttpLog>) -> Self {
+        Self {
+            topic,
+            tpl: TopicPartitionList::new(),
+            inserter,
+        }
+    }
+
+    // TODO: proper error type
+    // FIXME: error=error=Network(hyper::Error(IncompleteMessage))
+    #[instrument(skip(self, log))]
+    async fn write(&mut self, partition: i32, offset: i64, log: HttpLog) -> ImportResult<()> {
+        self.inserter
+            .write(&log)
+            .await
+            .expect("inserter write failed");
+
+        debug!(partition, offset, "written log data");
+
+        {
+            let mut p = match self.tpl.find_partition(&self.topic, partition) {
+                Some(p) => p,
+                None => self.tpl.add_partition(&self.topic, partition),
+            };
+
+            // XXX: offset + 1 (?)
+            p.set_offset(Offset::Offset(offset))
+                .expect("Kafka offset update");
+        }
+
+        // XXX: Is this pre-commit check even necessary?
+        let time_left = self.inserter.time_left();
+        let should_commit = time_left.map(|t| t.is_zero()).unwrap_or(false);
+
+        if !should_commit {
+            debug!(?time_left, partition, offset, "waiting to commit insert");
+            return ImportResult::Pending;
+        }
+
+        // XXX: handle retries? although, quite infeasible due to strict rate-limit
+        match self.inserter.commit().await {
+            // TODO: should commit only if q > 0
+            Ok(q) => {
+                info!(partition, quantities = ?q, "inserts committed");
+                // NOTE: commit is done only once in a while so the clone should be fine
+                ImportResult::Success(self.tpl.clone())
+            }
+            Err(e) => {
+                error!(partition, error = ?e, "insert commit failed: {:?}", e);
+                //error!(partition, error = ?e, "insert commit failed");
+                // panic!();
+                ImportResult::Failure(())
+            }
+        }
+    }
+}
+
 #[derive(Clone)]
 #[allow(unused)]
 struct Context {
     kafka: KafkaConfig,
     ch: Arc<clickhouse::Client>,
-    inserter: Arc<Mutex<LogInserter>>,
+    sink: Arc<Mutex<ClickHouseSink>>,
 }
 
 impl Context {
-    #[inline]
     fn new(kafka: KafkaConfig, ch: clickhouse::Client, inserter: LogInserter) -> Self {
+        let topic = kafka.topic.clone();
         Self {
             kafka,
             ch: Arc::new(ch),
-            inserter: Arc::new(Mutex::new(inserter)),
+            sink: Arc::new(Mutex::new(ClickHouseSink::new(topic, inserter))),
         }
     }
 }
@@ -194,8 +300,9 @@ fn create_consumer(cfg: KafkaConfig) -> rdkafka::error::KafkaResult<HttpLogConsu
         .set("enable.partition.eof", "false")
         .set("session.timeout.ms", "6000")
         .set("auto.offset.reset", "latest")
-        .set("enable.auto.commit", "true")
-        .set("auto.commit.interval.ms", "2000")
+        .set("enable.auto.commit", "false")
+        //.set("enable.auto.commit", "true")
+        //.set("auto.commit.interval.ms", "2000")
         .set("enable.auto.offset.store", "false")
         .set("isolation.level", "read_committed")
         .set("enable.auto.commit", "false")
@@ -207,6 +314,8 @@ fn create_consumer(cfg: KafkaConfig) -> rdkafka::error::KafkaResult<HttpLogConsu
     Ok(consumer)
 }
 
+// TODO: struct KafkaSource, struct HttpLogConsumer
+
 // XXX: delivery semantics => offset handling & idempotent write
 #[instrument(name = "consumer", skip(ctx))]
 async fn run_consumer(id: usize, ctx: Context) {
@@ -217,10 +326,10 @@ async fn run_consumer(id: usize, ctx: Context) {
 
     let stream_processor = consumer.stream().try_for_each(|msg| {
         let consumer = consumer.clone();
-        let inserter = ctx.inserter.clone();
+        let sink = ctx.sink.clone();
 
         async move {
-            // Process each message
+            // process a message
             let msg = msg.detach();
 
             let topic = msg.topic().to_owned();
@@ -229,64 +338,68 @@ async fn run_consumer(id: usize, ctx: Context) {
 
             debug!(parent: span, topic, partition, offset, "processing message");
 
-            let task_span = debug_span!("task");
+            // TODO: filter out invalid msgs (or rather report them to stats)
+            let mut log = HttpLog::try_from(msg).expect("valid message");
+            debug!(partition, offset, "message parsed");
 
-            tokio::spawn(async move {
-                let _enter = task_span.enter();
-                // executed on the main thread pool
+            log.remote_addr = anonymize_ip(log.remote_addr);
+            debug!(partition, offset, ?log, "anonymized");
 
-                // TODO: filter out invalid msgs (or rather report them to stats)
-                let mut log = HttpLog::try_from(msg).expect("valid message");
-                debug!(partition, offset, "message parsed");
-
-                log.remote_addr = anonymize_ip(log.remote_addr);
-                debug!(partition, offset, ?log, "anonymized");
-
-                // XXX: consider keeping just single consumer and scale the group horizontally
+            // XXX: consider keeping just single consumer and scale the group horizontally
+            let sink_span = debug_span!("sink");
+            let result = {
                 // critical write section
-                let insert_span = debug_span!("insert");
-                {
-                    let _enter = insert_span.enter();
+                let _enter = sink_span.enter();
+                let mut sink = sink.lock().await;
+                sink.write(partition, offset, log).await
+            };
 
-                    let mut out = inserter.lock().await;
+            match result {
+                ImportResult::Pending => {}
+                ImportResult::Success(tpl) => {
+                    // since offsets are sequential and commit retrospective, it should be
+                    // sufficient to commit just once after whole batch
 
-                    // FIXME: dev - this simulates data duplication, remove later
-                    out.write(&log).await.expect("inserter write failed");
-
-                    out.write(&log).await.expect("inserter write failed");
-                    debug!(partition, offset, "written log data");
-
-                    // FIXME: do not commit immediately => inefficient & hits proxy rate limiting
-                    match out.commit().await {
-                        Ok(q) => {
-                            info!(quantities = ?q, "inserts committed");
-                            // FIXME: saving out-of-order => potential data loss
-                            consumer
-                                .store_offset(&topic, partition, offset)
-                                // XXX: here we actually should terminate the process
-                                .expect("offsets stored");
-                        }
-                        Err(_) => error!("insert commit failed"),
-                    };
+                    // XXX: consider `CommitMode::Sync` (cons: blocks execution)
+                    // NOTE: since rate-limiting is 1 min, this might be more efficient than frequent
+                    // auto-commit of _stored_ offsets
+                    consumer
+                        .commit(&tpl, CommitMode::Async)
+                        // XXX: here we actually should terminate the process
+                        .expect("Kafka offsets committed");
                 }
-            });
+                ImportResult::Failure(()) => {
+                    //error!(partition, error = ?e, "insert commit failed");
+                    //panic!();
+                }
+            }
 
+            debug!(topic, partition, offset, "done processing message");
             Ok(())
         }
     });
 
-    info!("starting event loop");
+    info!("starting Kafka message stream processing");
     stream_processor.await.expect("stream processing failed");
     warn!("stream processing terminated");
 
     // TODO: graceful shutdown: terminate inserting
-    //inserter
-    //    .end()
-    //    .await
-    //    .expect("inserter shutdown flush failed");
+    //  - `inserter.end().await.expect("inserter shutdown flush failed");`
+    //  - also commit these offsets
 }
 
-// TODO: decide which crate to use to implement the pipeline (this is just a demo)
+// XXX: part of sink?
+#[instrument(skip(ch))]
+async fn make_table(ch: &clickhouse::Client, table: &str) {
+    ch.query(TABLE_DDL)
+        .bind(clickhouse::sql::Identifier(table))
+        .execute()
+        .await
+        .expect("making clickhouse table failed");
+
+    info!(table, "made sure log table exists in clickhouse");
+}
+
 // TODO: setup shutdown hook & graceful shutdown in general
 #[tokio::main]
 async fn main() {
@@ -302,13 +415,20 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    info!(tasks = cfg.workers, "starting anonymizer app");
+    info!(tasks = cfg.workers, "starting anonymizer pipeline");
 
     let ch = clickhouse::Client::from(&cfg.ch);
+    info!(
+        url = cfg.ch.url,
+        user = cfg.ch.user,
+        "clickhouse client created"
+    );
+
+    make_table(&ch, &cfg.ch.log_table).await;
 
     let inserter = ch
         .inserter(&cfg.ch.log_table) // NOTE: inserter name = table name
-        .expect("failed to create new inserter")
+        .expect("failed to create new clickhouse inserter")
         .with_max_entries(cfg.ch.max_entries)
         .with_period(cfg.ch.insert_period.map(Duration::from_secs));
 
